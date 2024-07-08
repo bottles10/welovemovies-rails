6e881553e25ae08809cb1b1ad8b061a9a0dc1d31
bottles10 <henryaovare@gmail.com>
2024-07-08 15:32:48 -0700

    Config minitest reporter and guard for testing

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,5 +4,7 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get welcome_home_url
     assert_response :success
+
+    assert_select 'h1', text: "Welcome#home"
   end
 end
